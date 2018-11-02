175a908f15316c32189dbe93378c3318cab6c4f3
Shiv Indap <indap.shiv@gmail.com>
2018-11-02 16:40:58 -0700

    CA-8481 Adding code to handle an exception from ServiceMonster when there is no data present, instead of returning an exception, we should be returning a response with count = 0 and items empty

--- lib/service_monster/response.rb
+++ lib/service_monster/response.rb
@@ -1,9 +1,21 @@
+# frozen_string_literal: true
+
 module ServiceMonster
   module Response
     def self.create(response_hash)
-      data = response_hash.data.dup rescue response_hash
+      data = begin
+               response_hash.data.dup
+             rescue StandardError
+               response_hash
+             end
       data.extend(self)
-      return data
+      data
+    end
+
+    def self.create_empty
+      data = { count: 0, items: [] }
+      data.extend(self)
+      data
     end
 
     attr_reader :pagination
